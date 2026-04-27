c1ade4087ce86b0d40d0797395a97102544bfcbf
Tomáš Milet <imilet@fit.vutbr.cz>
2026-04-27 17:04:52 +0200

    video player done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
+#include "geGL/DebugMessage.h"
+#include "opencv2/videoio.hpp"
 #include<iostream>
 
 #include<SDL3/SDL.h>
 #include<geGL/geGL.h>
 
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
 #include<geGL/StaticCalls.h>
 using namespace ge::gl;
 
 int main(){
-  auto window = SDL_CreateWindow("MUL2026",1024,768,SDL_WINDOW_OPENGL);
+  
+  cv::VideoCapture video;
+  cv::Mat bgrFrame;
+
+  video.open("../mtm.webm");
+
+  auto w = video.get(cv::CAP_PROP_FRAME_WIDTH );
+  auto h = video.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+  auto window = SDL_CreateWindow("MUL2026",w,h,SDL_WINDOW_OPENGL);
 
   auto context = SDL_GL_CreateContext(window);
 
   ge::gl::init();
+
+  ge::gl::setHighDebugMessage();
 
   auto vsSrc = R".(
   #version 460
@@ -35,7 +51,7 @@ int main(){
   layout(binding=0)uniform sampler2D frame;
 
   void main(){
-    fColor = texture(frame,vCoord);
+    fColor = texture(frame,vec2(vCoord.x,1-vCoord.y));
   }
   ).";
 
@@ -44,17 +60,10 @@ int main(){
 
   auto prg = std::make_shared<Program>(vs,fs);
 
-  float data[] = {
-    0,0,0,1,
-    1,0,1,1,
-    1,0,1,1,
-    0,0,0,1,
-  };
     
   GLuint frameTexture;
   glCreateTextures(GL_TEXTURE_2D,1,&frameTexture);
-  glTextureStorage2D(frameTexture,1,GL_RGBA32F,2,2);
-  glTextureSubImage2D(frameTexture,0,0,0,2,2,GL_RGBA,GL_FLOAT,data);
+  glTextureStorage2D(frameTexture,1,GL_RGB8,w,h);
   glTextureParameteri(frameTexture,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
   glTextureParameteri(frameTexture,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
 
@@ -66,6 +75,11 @@ int main(){
       if(event.type == SDL_EVENT_QUIT)running = false;
   
     }
+
+    video.read(bgrFrame);
+    glTextureSubImage2D(frameTexture,0,0,0,w,h,GL_BGR,GL_UNSIGNED_BYTE,bgrFrame.data);
+
+
     //rendering
     glClearColor(0.5,0.5,0.5,1);
     glClear(GL_COLOR_BUFFER_BIT);
